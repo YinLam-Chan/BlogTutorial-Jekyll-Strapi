150bb423a26237570dae5751d2251ebcbab3dedf
Victoria <valeriechan2002@yahoo.com.hk>
2021-10-19 03:21:25 +1300

    blog tutorial completed

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c0f9f33bd3320ecc197dfed1ad9e7c69'),
+    },
+  },
+});
